363e000ce921360efdb338a8967ef1221fecb9f9
MathiasGautier <mths.gautier@gmail.com>
2020-12-17 18:48:49 +0000

    changed gitignore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { datadogLogs } from '@datadog/browser-logs';
+
+datadogLogs.init({
+  clientToken: process.env.REACT_APP_SECRET, // Datadog client token
+  site: 'datadoghq.com',
+  env: 'logs_test',
+  forwardErrorsToLogs: false,
+  sampleRate: 100,
+  silentMultipleInit: true,
+})
+
+
+
+const logOne=()=>{
+  datadogLogs.logger.info('Log #1', { name: 'log 1 sent'});
+  console.log('sent log one')
+}
+
+const logTwo=()=>{
+  datadogLogs.logger.info('Log #2', { name: 'log 2 sent'});
+  console.log('sent log two')
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App-header">
+      <button onClick={logOne}>send log Numbr one</button>
+      <br></br>
+      <button onClick={logTwo}>send log Numbr two</button>
+      </div>
   );
 }
 
